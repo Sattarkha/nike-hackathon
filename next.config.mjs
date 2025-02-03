99395a27730dcc701a81df8c63329d7477bba836
abdul sattar <156591821+Sattarkha@users.noreply.github.com>
2025-02-03 18:46:34 +0500

    fixed next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.sanity.io'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['cdn.sanity.io', 'template-03-api.vercel.app'], // Only the domain, no `/api/products`
+  },
+};
+
+export default nextConfig;
